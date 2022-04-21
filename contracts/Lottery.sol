b920831db99807ebdfd003aa171933d971510558
Sijuade Ajagunna <mahyor.sam@gmail.com>
2022-04-21 09:35:38 +0100

    create base solidity contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,24 +4,54 @@ pragma solidity ^0.6.7;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract License {
+contract Lottery is Ownable {
     address payable[] public players;
     AggregatorV3Interface public priceFeed;
     uint256 usdEntranceFee;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE lottery_state;
 
-    constructor (address _priceFeed) public {
+    constructor(address _priceFeed) public {
         priceFeed = AggregatorV3Interface(_priceFeed);
-        usdEntranceFee = 50;
+        usdEntranceFee = 50 * (10**18);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
-    function enter () public payable {
+    function enter() public payable {
+        uint256 value = msg.value;
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "This lottery is currently closed"
+        );
+        require(
+            msg.value >= getEntranceFee(),
+            "More ETH is required to join this lottery"
+        );
         players.push(msg.sender);
     }
 
-    function getEntranceFee() public {}
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // pricefeed.decimals() is 8, 18 (magnitude of wei to eth) - 8 = 10
 
-    function startLottery () public {}
+        // convert usd entrance fee to wei equivalent
+        uint256 entranceFee = (usdEntranceFee * 10**18) / adjustedPrice;
+        return entranceFee;
+    }
 
-    function endLottery () public{}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "This lottery cannot be opened right now"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {}
 }
---
+++ tests/test_lottery.py
@@ -0,0 +1,19 @@
+from brownie import Lottery, accounts, network, config
+from web3 import Web3
+
+
+def test_get_entrace_fee():
+    account = accounts[0]
+    print(f"ntwork {network.show_active()}")
+    print(
+        f'sssssssssss {config["networks"][network.show_active()]["eth-usd-price-feed"]}'
+    )
+    lottery = Lottery.deploy(
+        config["networks"][network.show_active()]["eth-usd-price-feed"],
+        {"from": account},
+    )
+    entrance_fee = lottery.getEntranceFee()
+    print(f"entrance_fee {entrance_fee}")
+
+    assert entrance_fee > Web3.toWei(0.016, "ether")
+    assert entrance_fee < Web3.toWei(0.017, "ether")
