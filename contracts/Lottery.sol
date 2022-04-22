bfadef679dd8c89b38098ec3f3804cfe418b100d
Sijuade Ajagunna <mahyor.sam@gmail.com>
2022-04-22 12:24:40 +0100

    add chainlink vrf function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.6.7;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBase {
     address payable[] public players;
     AggregatorV3Interface public priceFeed;
     uint256 usdEntranceFee;
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    address payable lotteryWinner;
+
     enum LOTTERY_STATE {
         OPEN,
         CLOSED,
@@ -17,10 +24,18 @@ contract Lottery is Ownable {
     }
     LOTTERY_STATE lottery_state;
 
-    constructor(address _priceFeed) public {
+    constructor(
+        address _priceFeed,
+        address _vrfCoordinator,
+        address _link,
+        bytes32 _keyHash,
+        uint256 _fee
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         priceFeed = AggregatorV3Interface(_priceFeed);
         usdEntranceFee = 50 * (10**18);
         lottery_state = LOTTERY_STATE.CLOSED;
+        keyHash = _keyHash;
+        fee = _fee;
     }
 
     function enter() public payable {
@@ -53,5 +68,34 @@ contract Lottery is Ownable {
         lottery_state = LOTTERY_STATE.OPEN;
     }
 
-    function endLottery() public onlyOwner {}
+    function endLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "This lottery is currently not open"
+        );
+        // require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+
+        bytes32 requestId = requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "Lottery state not in CALCULATING WINNER"
+        );
+        require(randomness > 0, "This request has not been fulfilled");
+
+        uint256 playerIndex = randomness % players.length;
+
+        lotteryWinner = players[playerIndex];
+        lotteryWinner.transfer(address(this).balance);
+
+        lottery_state = LOTTERY_STATE.CLOSED;
+        players = new address payable[](0);
+        randomResult = randomness;
+    }
 }
